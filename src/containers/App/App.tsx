48ef26178e94c4b4254da7341eef2db196de9863
Piolo <piolot64@gmail.com>
2025-06-24 16:44:32 -0500

    Removed all styling

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,5 +1,4 @@
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { Route, Routes } from "react-router-dom";
 import { getPathMapping, stringToSlug } from "../../utils";
 import { useEffect } from "react";
